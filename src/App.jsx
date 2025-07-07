4bb07f872db66c4377b8206a88066a65fdc949c7
siyamregn77 <yeshidagnasiyamregn@gmail.com>
2025-07-08 01:10:15 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+function App() {
+
+  return (
+   <div>
+    <h1 className="text-3xl font-bold underline text-center my-4">
+      Hello world!
+    </h1>
+    <p className="text-gray-700 dark:text-gray-300">
+      This is a simple React app with Tailwind CSS.
+    </p>
+    
+   </div>
+  )
+}
+
+export default App
